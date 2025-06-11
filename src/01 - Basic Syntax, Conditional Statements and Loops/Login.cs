d1e25f6170ecfd1ba1666e3df8d94bfffcd5266f
nakovivan05 <ivannakov11@gmail.com>
2025-06-11 17:40:06 +0300

    feat(01-Basic Syntax, Conditional Statements and Loops): add Login exercise solution

---
+++ src/01 - Basic Syntax, Conditional Statements and Loops/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace SoftUniCSharp.BasicSyntaxConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 0;
+            string username = Console.ReadLine();
+            string reversed = "";
+            
+            for (int i = username.Length - 1; i >= 0; i--)
+            {
+                reversed += username[i];
+            }
+            while(true)
+            {
+                count++;
+                string attempt = Console.ReadLine();
+                if (attempt == reversed)
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    break;
+                }
+                if (count == 4)
+                {
+                    Console.WriteLine($"User {username} blocked!");
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+             
+            }
+        }
+    }
+}
