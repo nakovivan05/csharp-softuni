1403742e184d85f1129b4e69ce7a2f6342acb891
nakovivan05 <ivannakov11@gmail.com>
2025-06-11 17:43:39 +0300

    feat(01-Basic Syntax, Conditional Statements and Loops): add Strong Number exercise solution

---
+++ src/01 - Basic Syntax, Conditional Statements and Loops/StrongNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SoftUniCSharp.BasicSyntaxConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int save = num;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                int factoriel = 1;
+                for(int i = 1; i <= digit; i++)
+                {
+                    factoriel *= i;
+                }
+                sum += factoriel;
+                num /= 10;
+            }
+            if(save == sum)
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
