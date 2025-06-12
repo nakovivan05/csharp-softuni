75852b47f91534ec02df6f17a9d6e8db12cafed4
nakovivan05 <ivannakov11@gmail.com>
2025-06-12 22:50:11 +0300

    feat(01-Basic Syntax, Conditional Statements and Loops): add Vending Machine exercise solution

---
+++ src/01 - Basic Syntax, Conditional Statements and Loops/VendingMachine.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace SoftUniCSharp.BasicSyntaxConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            string input;
+            double totalMoney = 0.0;
+
+            while ((input = Console.ReadLine()) != "Start")
+            {
+                double coin = double.Parse(input);
+
+                if (coin == 0.1 || coin == 0.2 || coin == 0.5 || coin == 1 || coin == 2)
+                {
+                    totalMoney += coin;
+                }
+                else
+                {
+                    Console.WriteLine($"Cannot accept {coin}");
+                }
+            }
+
+            
+            while ((input = Console.ReadLine()) != "End")
+            {
+                double price = 0.0;
+
+                if (input == "Nuts")
+                {
+                    price = 2.0;
+                }
+                else if (input == "Water")
+                {
+                    price = 0.7;
+                }
+                else if (input == "Crisps")
+                {
+                    price = 1.5;
+                }
+                else if (input == "Soda")
+                {
+                    price = 0.8;
+                }
+                else if (input == "Coke")
+                {
+                    price = 1.0;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid product");
+                    continue;
+                }
+
+                if (totalMoney >= price)
+                {
+                    totalMoney -= price;
+                    Console.WriteLine($"Purchased {input.ToLower()}");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, not enough money");
+                }
+            }
+
+            Console.WriteLine($"Change: {totalMoney:F2}");
+        }
+    }
+}
