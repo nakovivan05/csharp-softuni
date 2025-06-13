84dba6c95b02585feea889a7ec85117b584fe2da
nakovivan05 <ivannakov11@gmail.com>
2025-06-13 17:45:06 +0300

    feat(01-Basic Syntax, Conditional Statements and Loops): add Padawan Equipment exercise solution

---
+++ src/01 - Basic Syntax, Conditional Statements and Loops/PadawanEquipment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SoftUniCSharp.BasicSyntaxConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            double money = double.Parse(Console.ReadLine());
+            int count = int.Parse(Console.ReadLine());
+            double priceLightsaber = double.Parse(Console.ReadLine());
+            double priceRobe = double.Parse(Console.ReadLine());
+            double priceBelt = double.Parse(Console.ReadLine());
+            double totalPriceLightsabers = Math.Ceiling(count + count * 0.1) * priceLightsaber;
+            double totalPriceRobes = count * priceRobe;
+            double totalPriceBelts = (count - count / 6) * priceBelt;
+            double totalPrice = totalPriceLightsabers + totalPriceRobes + totalPriceBelts;
+            if(totalPrice <= money)
+            {
+                Console.WriteLine($"The money is enough - it would cost {totalPrice:F2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"John will need {totalPrice - money:F2}lv more.");
+            }
+        }
+    }
+}
