3e1e202b018f1fb89292f31935283a9ac1a0da12
nakovivan05 <ivannakov11@gmail.com>
2025-06-13 17:48:41 +0300

    feat(01-Basic Syntax, Conditional Statements and Loops): add Orders exercise solution

---
+++ src/01 - Basic Syntax, Conditional Statements and Loops/Orders.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SoftUniCSharp.BasicSyntaxConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double totalPrice = 0;
+            for(int i = 0; i < n; i++)
+            {
+                double priceCapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int countCapsules = int.Parse(Console.ReadLine());
+                double price = (days * countCapsules) * priceCapsule;
+                totalPrice += price;
+                Console.WriteLine($"The price for the coffee is: ${price:F2}");
+            }
+            Console.WriteLine($"Total: ${totalPrice:F2}");
+        }
+    }
+}
