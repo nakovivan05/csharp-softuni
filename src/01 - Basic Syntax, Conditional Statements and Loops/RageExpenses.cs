8781b987a7f96754da5723ca5fd81219c5fb1739
nakovivan05 <ivannakov11@gmail.com>
2025-06-13 17:47:12 +0300

    feat(01-Basic Syntax, Conditional Statements and Loops): add Rage Expenses exercise solution

---
+++ src/01 - Basic Syntax, Conditional Statements and Loops/RageExpenses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SoftUniCSharp.BasicSyntaxConditionalStatementsAndLoops
+{
+    class Program
+    {
+        static void Main()
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double priceHeadset = double.Parse(Console.ReadLine());
+            double priceMouse = double.Parse(Console.ReadLine());
+            double priceKeyboard = double.Parse(Console.ReadLine());
+            double priceDisplay = double.Parse(Console.ReadLine());
+            double expenses = (lostGames / 2) * priceHeadset + (lostGames/3)*priceMouse + (lostGames/6)*priceKeyboard + (lostGames/12)*priceDisplay;
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+        }
+    }
+}
