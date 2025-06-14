5d391d902f4cf53e3d9cbf80dbcc303dab0f788c
nakovivan05 <ivannakov11@gmail.com>
2025-06-14 22:10:39 +0300

    feat(02-Data Types and Variables): add Sum Digits exercise solution

---
+++ src/02 - Data Types and Variables/SumDigits.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace SoftuniCSharpDataTypesAndVariables
+{
+    class SumDigits
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sumOfDigits = 0;
+            while (num != 0)
+            {
+                int digit = num % 10;
+                sumOfDigits += digit;
+                num /= 10;
+            }
+            Console.WriteLine(sumOfDigits);
+
+        }
+    }
+}
