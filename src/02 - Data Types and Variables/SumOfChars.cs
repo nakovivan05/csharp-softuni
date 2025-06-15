181b16f054a5f3ac9550181a4ab586c729db0600
nakovivan05 <ivannakov11@gmail.com>
2025-06-15 22:45:22 +0300

    feat(02 - Data Types and Variables): add Sum of Chars exercise solution

---
+++ src/02 - Data Types and Variables/SumOfChars.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SoftuniCSharpDataTypesAndVariables
+{
+    class SumOfChars
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int totalSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char letter = char.Parse(Console.ReadLine());
+                totalSum += (int)letter;
+            }
+            Console.WriteLine($"The sum equals: {totalSum}");
+        }
+    }
+}
