28a4cbba5f673355713a66e42154fd26688e0487
nakovivan05 <ivannakov11@gmail.com>
2025-06-16 23:55:09 +0300

    feat(02-Data Types and Variables): add Water Overflow exercise solution

---
+++ src/02 - Data Types and Variables/WaterOverflow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 255;
+            int n = int.Parse(Console.ReadLine());
+            int poured = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int quantity = int.Parse(Console.ReadLine());
+                if(poured + quantity > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                poured += quantity;
+            }
+            Console.WriteLine(poured);
+        }
+    }
+}
