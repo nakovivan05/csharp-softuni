29616d3fc22e2c12a14373bc26db11c746387d5f
nakovivan05 <ivannakov11@gmail.com>
2025-06-17 22:44:23 +0300

    feat(02-Data Types and Variables): add Beer Kegs exercise solution

---
+++ src/02 - Data Types and Variables/BeerKegs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SoftuniCSharpDataTypesAndVariables
+{
+    class BeerKegs
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string biggestKeg = "";
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * radius*radius*height;
+                if(volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    biggestKeg = model;
+                }
+            }
+            Console.WriteLine(biggestKeg);
+        }
+    }
+}
