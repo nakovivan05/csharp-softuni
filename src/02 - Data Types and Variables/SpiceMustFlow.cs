15d6e7cf8d87efe086ae1922fc6c19854fed4136
nakovivan05 <ivannakov11@gmail.com>
2025-06-17 22:49:18 +0300

    feat(02-Data Types and Variables): add Spice Must Flow exercise solution

---
+++ src/02 - Data Types and Variables/SpiceMustFlow.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SoftuniCSharpDataTypesAndVariables
+{
+    class SpiceMustFlow
+    {
+        static void Main()
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int amountOfSpice = 0;
+            int countOfDays = 0;
+            while(startingYield >= 100)
+            {
+                countOfDays++;
+                amountOfSpice += startingYield;
+                amountOfSpice -= 26;
+                startingYield -= 10;
+            }
+            if(amountOfSpice>=26)
+            {
+                amountOfSpice -= 26;
+            }
+            else
+            {
+                amountOfSpice = 0;
+            }
+            Console.WriteLine(countOfDays);
+            Console.WriteLine(amountOfSpice);
+
+        }
+    }
+}
