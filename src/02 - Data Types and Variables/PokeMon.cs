5f00714d31666b486efa42c3ff5024b6a9069271
nakovivan05 <ivannakov11@gmail.com>
2025-06-17 22:50:03 +0300

    feat(02-Data Types and Variables): add PokeMon exercise solution

---
+++ src/02 - Data Types and Variables/PokeMon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SoftuniCSharpDataTypesAndVariables
+{
+    class PokeMon
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int countOfTargets = 0;
+            int save = n;
+            while (n >= m)
+            {
+                n -= m;
+                if(n == save/2 && y!=0)
+                {
+                    n /= y;
+                    
+                }
+                countOfTargets++;
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(countOfTargets);
+
+
+        }
+    }
+}
