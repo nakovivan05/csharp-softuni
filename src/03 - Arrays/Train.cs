3ae47a24392754066f37a59bc00700e03b3803b6
nakovivan05 <ivannakov11@gmail.com>
2025-06-19 17:19:04 +0300

    feat(03-Arrays): add Train exercise solution

---
+++ src/03 - Arrays/Train.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpArrays
+{
+    class Train
+    {
+        static void Main()
+        {
+            int countOfWagons = int.Parse(Console.ReadLine());
+            int countOfPassengers = 0;
+            int[]passengers = new int[countOfWagons];
+            for(int i = 0; i < countOfWagons; i++)
+            {
+                int passengersCurrentWagon = int.Parse(Console.ReadLine());
+                passengers[i] = passengersCurrentWagon;
+            }
+            for(int i = 0;i < countOfWagons; i++)
+            {
+                Console.Write(passengers[i] + " ");
+                countOfPassengers += passengers[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine(countOfPassengers);
+        }
+    }
+}
