e2d228e67101922b9df4097588a5caefc1bf7293
nakovivan05 <ivannakov11@gmail.com>
2025-06-19 17:20:22 +0300

    feat(03-Arrays): add Common Elements exercise solution

---
+++ src/03 - Arrays/CommonElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpArrays
+{
+    class CommonElements
+    {
+        static void Main()
+        {
+            string[] firstArray = Console.ReadLine().Split(' ');
+            string[] secondArray = Console.ReadLine().Split(' ');
+            foreach (string item in firstArray)
+            {
+                foreach (string item2 in secondArray)
+                {
+                    if(item == item2)
+                    {
+                        Console.Write(item + " ");
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
