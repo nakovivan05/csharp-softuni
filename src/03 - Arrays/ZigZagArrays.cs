b1dcc773d2fa1ca4bc7bb8debeea247722623c5c
nakovivan05 <ivannakov11@gmail.com>
2025-06-20 22:48:42 +0300

    feat(03-Arrays): add Zig-Zag Arrays exercise solution

---
+++ src/03 - Arrays/ZigZagArrays.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpArrays
+{
+    class ZigZagArrays
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] firstArray = new int[n];
+            int[] secondArray = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                var input = Console.ReadLine();
+                int[] twoIntegers = input.Split(' ').Select(int.Parse).ToArray();
+                if (i % 2 == 0)
+                {
+                    firstArray[i] = twoIntegers[0];
+                    secondArray[i] = twoIntegers[1];
+                }
+                else
+                {
+                    firstArray[i] = twoIntegers[1];
+                    secondArray[i] = twoIntegers[0];
+                }
+
+            }
+            foreach(int integer in firstArray)
+            {
+                Console.Write(integer + " ");
+            }
+            Console.WriteLine();
+            foreach(int integer in secondArray)
+            {
+                Console.Write(integer + " ");
+            }
+        }
+    }
+}
