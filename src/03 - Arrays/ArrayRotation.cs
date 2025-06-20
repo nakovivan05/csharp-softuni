ecfb518a3676fc87d1a735ae72a72bacb8d6cd19
nakovivan05 <ivannakov11@gmail.com>
2025-06-20 22:51:03 +0300

    feat(03-Arrays): add Array Rotation exercise solution

---
+++ src/03 - Arrays/ArrayRotation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpArrays
+{
+    class ArrayRotation
+    {
+        static void Main()
+        {
+            int[]arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int countOfRotations = int.Parse(Console.ReadLine());
+            for (int i = 0; i < countOfRotations; i++)
+            {
+                int first = arr[0];
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    arr[j] = arr[j+1];
+                }
+                arr[arr.Length-1] = first;
+            }
+            Console.WriteLine(string.Join(" ",arr));
+        }
+    }
+}
