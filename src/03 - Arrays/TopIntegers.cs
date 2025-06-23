fbfe98356bee321c56e8184283ccff7c251b183c
nakovivan05 <ivannakov11@gmail.com>
2025-06-23 22:22:35 +0300

    feat(03-Arrays): add Top Integers exercise solution

---
+++ src/03 - Arrays/TopIntegers.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpArrays
+{
+    class TopIntegers
+    {
+        static void Main()
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool isGreater = true;
+                if (i != arr.Length - 1)
+                {
+                    for (int j = i + 1; j < arr.Length; j++)
+                    {
+                        if (arr[j] >= arr[i])
+                        {
+                            isGreater = false;
+                            break;
+                        }
+                    }
+                    if (isGreater)
+                    {
+                        Console.Write(arr[i] + " ");
+                    }
+                }
+                else
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+        }
+        
+    }
+}
