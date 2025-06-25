078bd060d842aa195ae6b858b3ff0ced7fbbd00a
nakovivan05 <ivannakov11@gmail.com>
2025-06-25 18:39:29 +0300

    feat(04-Methods): add Vowels Count exercise solution

---
+++ src/04 - Methods/VowelsCount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpMethods
+{
+    class VowelsCount
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+            int foundVowels = findCountOfVowels(word);
+            Console.WriteLine(foundVowels);
+        }
+        static int findCountOfVowels(string word)
+        {
+            char[] vowels = { 'a', 'e', 'o', 'u', 'i', 'A', 'E', 'O', 'U', 'I' };
+            int foundVowels = 0;
+            for(int i = 0; i < word.Length; i++)
+            {
+                for(int j = 0; j<vowels.Length;j++)
+                {
+                    if(word[i] == vowels[j])
+                    {
+                        foundVowels++;
+                        break;
+                    }
+                }
+            }
+            return foundVowels;
+        }
+    }
+}
