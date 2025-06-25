21005baa4d790fd86234852b16e0851c43ca96c6
nakovivan05 <ivannakov11@gmail.com>
2025-06-25 18:38:45 +0300

    feat(04-Methods): add Smallest Of Three Numbers exercise solution

---
+++ src/04 - Methods/SmallestOfThreeNumbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpMethods
+{
+    class SmallestOfThreeNumbers
+    {
+        
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int smallest = findSmallestOfThreeNumbers(a, b, c);
+            Console.WriteLine(smallest);
+        }
+        static int findSmallestOfThreeNumbers (int a, int b, int c)
+        {
+            if (a < b && a < c)
+            {
+                return a;
+            }
+            else if (b < c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+    }
+}
