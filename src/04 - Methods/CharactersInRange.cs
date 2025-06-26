d74edd2eb9291ad1928eeb1ddc222dad70dc7746
nakovivan05 <ivannakov11@gmail.com>
2025-06-26 22:39:24 +0300

    feat(04-Methods): add Characters in Range exercise solution

---
+++ src/04 - Methods/CharactersInRange.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace SoftUniCSharpMethods
+{
+    class CharactersInRange
+    {
+        static void Main()
+        {
+            char start = char.Parse(Console.ReadLine());
+            char end = char.Parse(Console.ReadLine());
+            showCharactersInGivenRange(start, end);
+        }
+        static void showCharactersInGivenRange(char start, char end)
+        {
+            if (start < end)
+            {
+                for(int i = start + 1; i < end;i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+            }
+            else
+            {
+                char swap;
+                swap = start;
+                start = end;
+                end = swap;
+                for (int i = start + 1; i < end; i++)
+                {
+                    Console.Write((char)i + " ");
+                }
+            }
+        }
+    }
+}
