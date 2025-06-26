100b201d3abc4fdf1672123bbb5071ab45545fb1
nakovivan05 <ivannakov11@gmail.com>
2025-06-26 22:40:27 +0300

    feat(04-Methods): add Password Validator exercise solution

---
+++ src/04 - Methods/PasswordValidator.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+
+namespace SoftUniCSharpMethods
+{
+    class PasswordValidator
+    {
+        static void Main()
+        {
+            string password = Console.ReadLine();
+            if (isPasswordLengthValid(password) && isPasswordCharactersValid(password) && isPasswordDigitsValid(password))
+            {
+                Console.WriteLine("Password is valid");
+            }
+            else
+            {
+                if (!isPasswordLengthValid(password))
+                {
+                    Console.WriteLine("Password must be between 6 and 10 characters");
+                }
+                if (!isPasswordCharactersValid(password))
+                {
+                    Console.WriteLine("Password must consist only of letters and digits");
+                }
+                if(!isPasswordDigitsValid(password))
+                {
+                    Console.WriteLine("Password must have at least 2 digits");
+                }
+            }
+        }
+        static bool isPasswordLengthValid(string password)
+        {
+            return password.Length >= 6 && password.Length <= 10;
+        }
+        static bool isPasswordCharactersValid(string password)
+        {
+            foreach (char letter in password)
+            {
+                if (!char.IsLetterOrDigit(letter))
+                {
+                    return false;
+                    
+                }
+                
+            }
+            return true;
+        }
+        static bool isPasswordDigitsValid(string password)
+        {
+            int countOfDigits = 0;
+            foreach (char letter in password)
+            {
+                if(char.IsDigit(letter))
+                {
+                    countOfDigits++;
+                }
+            }
+            return countOfDigits >= 2;
+        }
+
+    }
+}
