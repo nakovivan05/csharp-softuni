b775725ce783e222bb273ed9acf9387e5e72140c
nakovivan05 <ivannakov11@gmail.com>
2025-06-27 18:50:28 +0300

    feat(04-Methods): add Middle Characters exercise solution

---
+++ src/04 - Methods/MiddleCharacters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace SoftUniCSharpMethods
+{
+    class MiddleCharacters
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+            findMiddleCharacter(word);
+        }
+        static void findMiddleCharacter(string word)
+        {
+            if(word.Length % 2 == 0)
+            {
+                Console.Write($"{word[word.Length / 2 - 1]}{word[word.Length / 2]}");
+            }
+            else
+            {
+                Console.Write(word[word.Length / 2]);
+            }
+        }
+    }
+}
