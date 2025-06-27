457260a0607ba4c65a8acfd7f029a54e8dde4537
nakovivan05 <ivannakov11@gmail.com>
2025-06-27 18:49:31 +0300

    feat(04-Methods): add Add and Substract exercise solution

---
+++ src/04 - Methods/AddAndSubstract.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace SoftUniCSharpMethods
+{
+    class AddAndSubstarct
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int result = substract(findSum(a, b), c);
+            Console.WriteLine(result);
+        }
+        static int findSum(int a, int b)
+        {
+            return a + b;
+        }
+        static int substract(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
