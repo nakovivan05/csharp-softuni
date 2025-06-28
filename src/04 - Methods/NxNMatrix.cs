a9816faab89b94782af444c29b7f1515063fda52
nakovivan05 <ivannakov11@gmail.com>
2025-06-28 17:48:11 +0300

    feat(04-Methods): add NxN Matrix exercise solution

---
+++ src/04 - Methods/NxNMatrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace SoftUniCSharpMethods
+{
+    class CommonElements
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            createSquareMatrix(n);
+        }
+        static void createSquareMatrix(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(n + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
