921b318388ba6f2a55ac61977776fc74b6e56ace
nakovivan05 <ivannakov11@gmail.com>
2025-06-28 17:49:43 +0300

    feat(04-Methods): add Factorial Division exercise solution

---
+++ src/04 - Methods/FactorialDivision.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace SoftUniCSharpMethods
+{
+    class FactorialDivision
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            divideFactorialsOfTwoIntegers(a, b);
+        }
+        static void  divideFactorialsOfTwoIntegers(int a, int b)
+        {
+            long factorialA = 1;
+            long factorialB = 1;
+            while(a>0)
+            {
+                factorialA *= a;
+                a--;
+            }
+            while(b>0)
+            {
+                factorialB *= b;
+                b--;
+            }
+            Console.WriteLine($"{(double)factorialA / factorialB:F2}");
+        }
+    }
+}
