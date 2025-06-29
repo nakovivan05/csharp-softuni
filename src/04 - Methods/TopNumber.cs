da93219a84568af9be4768b6bfad76c31769e38e
nakovivan05 <ivannakov11@gmail.com>
2025-06-29 22:48:42 +0300

    feat(04-Methods): add Top Number exercise solution

---
+++ src/04 - Methods/TopNumber.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace SoftUniCSharpMethods
+{
+    class TopNumber
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                if(isItTopNumber(i))
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        static bool isItTopNumber(int num)
+        {
+            int sumOfDigits = 0;
+            bool hasOddDigit = false;
+            char[] oddDigits = { '1','3','5','7','9' };
+            string number = num.ToString();
+            while (num > 0)
+            {
+                int digit = num % 10;
+                sumOfDigits += digit;
+                num /= 10;
+            }
+            
+            for (int i = 0; i < number.Length; i++)
+            {
+                for (int j = 0; j < oddDigits.Length; j++)
+                {
+                    if (number[i] == oddDigits[j])
+                    {
+                        hasOddDigit = true;
+                        break;
+                    }
+                }
+            }
+            return sumOfDigits%8==0 && hasOddDigit;
+        }
+    }
+}
