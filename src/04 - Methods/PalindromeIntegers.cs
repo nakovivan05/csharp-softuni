4675c4598cf80495f3094d22cc80218c500ba0bd
nakovivan05 <ivannakov11@gmail.com>
2025-06-29 22:47:51 +0300

    feat(04-Methods): add Palindrome Integers exercise solution

---
+++ src/04 - Methods/PalindromeIntegers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SoftUniCSharpMethods
+{
+    class PalindromeIntegers
+    {
+        static void Main()
+        {
+            string command = "";
+            while((command = Console.ReadLine())!= "END")
+            {
+                int num = int.Parse(command);
+                Console.WriteLine(isItPalindrome(num));
+            }
+        }
+        static bool isItPalindrome(int num)
+        {
+            string number = num.ToString();
+            string reversed = "";
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                reversed += number[i];
+            }
+            return reversed == number;
+        }
+    }
+}
