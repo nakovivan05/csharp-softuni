d7fc5fcccb74dea6817aa8b6f882965b814f827c
nakovivan05 <ivannakov11@gmail.com>
2025-06-30 22:53:11 +0300

    feat(05-Lists): add Change List exercise solution

---
+++ src/05 - Lists/ChangeList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SoftUniCSharpLists
+{
+    class ChangeList
+    {
+        static void Main()
+        {
+            List<int> list = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            string command = "";
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] words = command.Split();
+                if (words[0]=="Delete")
+                {
+                    int element = int.Parse(words[1]);
+                    list.RemoveAll(n => n == element);
+                }
+                else if(words[0]=="Insert")
+                {
+                    int element = int.Parse(words[1]);
+                    int position = int.Parse(words[2]);
+                    list.Insert(position,element);
+                }
+            }
+            Console.WriteLine(string.Join(" ", list));
+
+        }
+    }
+}
