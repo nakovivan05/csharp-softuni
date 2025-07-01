485f010c5492aec3ffca420df671775acdc79385
nakovivan05 <ivannakov11@gmail.com>
2025-07-01 22:32:05 +0300

    feat(05-Lists): add House Party exercise solution

---
+++ src/05 - Lists/HouseParty.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SoftUniCSharpLists
+{
+    class HouseParty
+    {
+        static void Main()
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            List<string> names = new List<string>();
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string input = Console.ReadLine();
+                string[] command = input.Split();
+                if (command[2] == "not")
+                {
+                    if (names.Contains(command[0]))
+                    {
+                        names.Remove(command[0]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{command[0]} is not in the list!");
+                    }
+                }
+                else
+                {
+                    if (!names.Contains(command[0]))
+                    {
+                        names.Add(command[0]);
+
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{command[0]} is already in the list!");
+                    }
+
+                }
+            }
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+
+    }
+
+}
