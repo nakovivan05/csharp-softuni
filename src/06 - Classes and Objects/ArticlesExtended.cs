1adf72ebdc19909ed7fe48be33851e1d5702dafb
nakovivan05 <ivannakov11@gmail.com>
2025-07-07 23:06:37 +0300

    feat(06-Classes and Objects): add Articles 2.0 exercise solution

---
+++ src/06 - Classes and Objects/ArticlesExtended.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SoftUniCSharpClassesAndObjects
+{
+    class Articles
+    {
+        public string Title { get; private set; }
+        public string Content { get; private set; }
+        public string Author { get; private set; }
+
+        public Articles(string title, string content, string author)
+        {
+            Title = title;
+            Content = content;
+            Author = author;
+        }
+        public override string ToString()
+        {
+            return $"{Title} - {Content}: {Author}";
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfArticles = int.Parse(Console.ReadLine());
+            List<string> articles = new List<string>();
+            for (int i = 0; i < numberOfArticles; i++)
+            {
+                string input = Console.ReadLine();
+                string[]article = input.Split(", ").ToArray();
+                string title = article[0];
+                string content = article[1];
+                string author = article[2];
+                Articles newArticle = new Articles(title,content,author);
+                articles.Add(newArticle.ToString());
+            }
+            for (int i = 0; i < articles.Count(); i++)
+            {
+                Console.WriteLine(articles[i]);
+            }
+        }
+    }
+}
+
+
