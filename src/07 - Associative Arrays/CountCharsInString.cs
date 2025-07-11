ecd285b3f9298808ee8a81aab7faf812c4f013bb
nakovivan05 <ivannakov11@gmail.com>
2025-07-11 22:44:18 +0300

    feat(07-Associative Arrays): add Count Chars in a String exercise solution

---
+++ src/07 - Associative Arrays/CountCharsInString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SoftUniCSharpAssociativeArrays
+{
+    class CountCharsInString
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+            var counts = new Dictionary<char, int>();
+            var order = new List<char>();
+            foreach (char c in input)
+            {
+                if (c == ' ')
+                {
+                    continue;
+                }
+                if (!counts.ContainsKey(c))
+                {
+                    counts[c] = 0;
+                    order.Add(c);
+                }
+                counts[c]++;
+            }
+            foreach (char c in order)
+            {
+                Console.WriteLine($"{c} -> {counts[c]}");
+            }
+        }
+    }
+}
