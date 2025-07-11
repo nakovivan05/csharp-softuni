4234fdb645952fe869f1cfb3dc4ed3e45ec9dc97
nakovivan05 <ivannakov11@gmail.com>
2025-07-11 22:45:17 +0300

    feat(07-Associative Arrays): add A Miner Task exercise solution

---
+++ src/07 - Associative Arrays/MinerTask.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoftUniCSharpAssociativeArrays
+{
+    public class MinerTask
+    {
+        static void Main()
+        {
+            var quantities = new Dictionary<string, long>();
+            var order = new List<string>();
+            while (true)
+            {
+                string resource = Console.ReadLine();
+                if(resource == "stop")
+                {
+                    break;
+                }
+                long quantity = long.Parse(Console.ReadLine());
+                if (!quantities.ContainsKey(resource))
+                {
+                    quantities[resource] = 0;
+                    order.Add(resource);
+                }
+                quantities[resource] += quantity;
+            }
+            foreach(var s in order)
+            {
+                Console.WriteLine($"{s} -> {quantities[s]}");
+            }
+            
+        }
+    }
+}
