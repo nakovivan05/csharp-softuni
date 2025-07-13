1b34906a0026053eaa90c2455529b2ea0477fbeb
nakovivan05 <ivannakov11@gmail.com>
2025-07-13 22:22:01 +0300

    feat(07-Associative Arrays): add Courses exercise solution

---
+++ src/07 - Associative Arrays/Courses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SoftUniCSharpAssociativeArrays
+{
+    public class Courses
+    {
+        static void Main()
+        {
+            var courses = new Dictionary<string,List<string>>();
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] courseInformation = input.Split(" : ").ToArray();
+                string courseName = courseInformation[0];
+                string courseParticipant = courseInformation[1];
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses[courseName] = new List<string>();
+                }
+                courses[courseName].Add(courseParticipant);
+
+            }
+            foreach(var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach(var student in course.Value)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+
+        }
+    }
+}
