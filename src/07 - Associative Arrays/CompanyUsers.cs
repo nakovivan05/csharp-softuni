622647675dae2c3070abed8e03c97efb48171cfe
nakovivan05 <ivannakov11@gmail.com>
2025-07-13 22:24:05 +0300

    feat(07-Associative Arrays): add Company Users exercise solution

---
+++ src/07 - Associative Arrays/CompanyUsers.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+namespace SoftUniCSharpAssociativeArrays
+{
+    public class CompanyUsers
+    {
+        static void Main()
+        {
+            var companyEmployees = new Dictionary<string, List<string>>();
+            var companyNames = new List<string>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[]newEmployee = input.Split(" -> ").ToArray();
+                string companyName = newEmployee[0];
+                string employeeID = newEmployee[1];
+                if (!companyEmployees.ContainsKey(companyName))
+                {
+                    companyEmployees[companyName] = new List<string>();
+                    companyNames.Add(companyName);
+                }
+                if (!companyEmployees[companyName].Contains(employeeID))
+                {
+                    companyEmployees[companyName].Add(employeeID);
+                }
+            }
+            foreach(var company in companyNames)
+            {
+                Console.WriteLine(company);
+                foreach (var employee in companyEmployees[company])
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
+        }
+    }
+}
