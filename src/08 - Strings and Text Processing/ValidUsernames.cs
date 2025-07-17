fa9a3d6aaae39e677545bfceca7bea81c57923ec
nakovivan05 <ivannakov11@gmail.com>
2025-07-17 21:54:24 +0300

    feat(08-Strings and Text Processing): add Valid Usernames exercise solution

---
+++ src/08 - Strings and Text Processing/ValidUsernames.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUniCSharpStringsAndTextProcessing
+{
+    public class ValidUsernames
+    {
+        static void Main()
+        {
+            string[]usernames = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).ToArray();
+            foreach (string username in usernames)
+            {
+                if(isValidUsername(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+        static bool isValidUsername(string username)
+        {
+            if (username.Length < 3 || username.Length > 16)
+            {
+                return false;
+            }
+            foreach (char c in username)
+            {
+                if (!(char.IsLetterOrDigit(c) || c == '-' || c == '_'))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        }
+}
