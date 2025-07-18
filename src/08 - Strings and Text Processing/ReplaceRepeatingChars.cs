c895a88aba1aa228978da1317d0c4ed98b50e3b0
nakovivan05 <ivannakov11@gmail.com>
2025-07-18 22:47:28 +0300

    feat(08-Strings and Text Processing): add Replace Repeating Chars exercise solution

---
+++ src/08 - Strings and Text Processing/ReplaceRepeatingChars.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+
+namespace SoftUniCSharpStringsAndTextProcessing
+{
+    public class MultiplyBigNumber
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            char previousChar = text[0];
+            sb.Append(previousChar);
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i]!=previousChar)
+                {
+                    sb.Append(text[i]);
+                    previousChar = text[i];
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
