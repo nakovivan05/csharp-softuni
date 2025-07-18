7fbbb71323614e72c22e3f47e152e112c90652fa
nakovivan05 <ivannakov11@gmail.com>
2025-07-18 22:46:29 +0300

    feat(08-Strings and Text Processing): add Caesar Cipher exercise solution

---
+++ src/08 - Strings and Text Processing/CaesarCipher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+
+namespace SoftUniCSharpStringsAndTextProcessing
+{
+    public class CaesarCipher
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+            StringBuilder sb = new StringBuilder();
+            foreach(char c in text)
+            {
+                sb.Append((char)(c+3));
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
