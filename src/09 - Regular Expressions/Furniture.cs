ae934948c32c37a9abc1b564aaf4478e7f8e22b0
nakovivan05 <ivannakov11@gmail.com>
2025-08-08 18:20:58 +0300

    feat(09-Regular Expressions): add Furniture exercise solution

---
+++ src/09 - Regular Expressions/Furniture.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace SoftUniCSharpRegularExpressions
+{
+    class Furniture
+    {
+        static void Main()
+        {
+            string input;
+            string pattern = @"[>]{2}(?<name>[A-Z][a-zA-z]+)[<]{2}(?<price>[0-9]+\.*[0-9]*)\!(?<quantity>[0-9]+)";
+            List<string> furniture = new List<string>();
+            double totalPrice = 0;
+            while ((input = Console.ReadLine()) != "Purchase")
+            {
+                Match match = Regex.Match(input,pattern);
+                if(match.Success)
+                {
+                    string name = match.Groups["name"].Value;
+                    double price = double.Parse(match.Groups["price"].Value);
+                    int quantity = int.Parse(match.Groups["quantity"].Value);
+                    furniture.Add(name);
+                    totalPrice += price * quantity;
+                }
+            }
+            Console.WriteLine("Bought furniture:");
+            foreach (string f in furniture)
+            {
+                Console.WriteLine(f);
+            }
+            Console.WriteLine($"Total money spend: {totalPrice:F2}");
+
+        }
+    }
+}
